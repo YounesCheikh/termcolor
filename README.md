c18eaa7c2dd7c60bbd67aeaddb8745b810e48718
cYounes <younes.cheikh@gmail.com>
2012-11-12 15:43:28 +0100

    add examples to readme file

--- README.md
+++ README.md
@@ -1,45 +1,236 @@
-termcolor
+Termcolor
 =========
 
-ANSII Color formatting for terminal output
+Is a C library used for ANSII Color formatting for terminal output.
 
-### Available Colors:
-+ BLACK
-+ RED
-+ GREEN
-+ YELLOW
-+ BLUE
-+ MAGENTA
-+ CYAN
-+ WHITE
-and: 
-+ DEFAULT 
+![Termcolor Library](http://localhost/examples/example_termcolor.png) 
 
-### BOOLEAN ?
-Use  `TRUE` and `FALSE` 
+## Available Colors:
+ `BLACK` `RED` `GREEN` `YELLOW` `BLUE` `MAGENTA` `CYAN` `WHITE`
 
-### Functions:
+
+## BOOLEAN ?
+By including **termcolor** library in your code, you don't need to include the ~~**stdbool**~~ library. However you need to write the boolean keywords in uppercase characters:  ***TRUE***  and ***FALSE*** 
+
+## Functions:
+
+#### Colors: 
 
 + `bgColor(COLOR)` : set the background color using the available colors.
 
 + `fgColor(COLOR)` : set the foreground color using the available colors.
 
-+ `textBold(BOOLEAN)` : text bold
+for `bgColor(COLOR)` and `fgColor(COLOR)` functions use the variable `DEFAULT`as parameter to reset the default color:
+```
+{
+	…
+	bgColor(DEFAULT);
+	fgColor(DEFAULT);
+	…
+}
+```
+#### Text decorations:
 
-+ `textBlink(BOOLEAN)` : blink text
++ `textBold(BOOLEAN)` : enable or disable text bolding for the next output:
+``` 
+	{
+		…
+		textBold(TRUE); 
+		…
+		textBold(FALSE);
+	} 
+```
 
-+ `colorReverse(BOOLEAN)`: reverse colors
++ `textBlink(BOOLEAN)` : enable or disable text blinking for the next output:
+``` 
+	{
+		...
+		textBlink(TRUE); 
+		...
+		textBlink(FALSE);
+	} 
+```
+
++ `colorReverse(BOOLEAN)`: enable or disable colors reversing for the next output:
+``` 
+	{
+		...
+		colorReverse(TRUE); 
+		...
+		colorReverse(FALSE);
+	} 
+```
 
 + `textUnderline(BOOLEAN)`: underline text
 
-+ `highFgIntensity(BOOLEAN)` : ...
++ `highFgIntensity(BOOLEAN)` : Use High Intensity for foreground colors.
 
-+ `highBgIntensity(BOOLEAN)` : ...
++ `highBgIntensity(BOOLEAN)` : Use High Intensity for background colors.
 
-+ `setStyle()` : ...
++ `setStyle()` : Usually the programer don't need to invoke this function, it will be invoked automatically by the other functions each time you change the style.
 
-+ `resetStyle()` : ...
++ `resetStyle()` : Reset all colors and decorations by default!, i segues to invoke this function at the end of your main to restore all as default.
+
+### Examples:
+##### Red Background:
+
+```
+{
+	bgColor(RED);
+	printf("text with RED Background");
+}
+```
+######Result:
+![termcolor red background](http://localhost/examples/red_background.png) 
+-
+##### Default background + Red foreground:
+
+```
+{
+	bgColor(DEFAULT);
+	fgColor(RED);
+	printf("text with RED Foreground");
+}
+```
+######Result:
+![termcolor red foreground](http://localhost/examples/red_foreground.png)
+-
+##### Yellow text on red background:
+
+```
+{
+	bgColor(RED);
+	fgColor(YELLOW);
+	printf("YELLOW text in RED Background");
+}
+```
+######Result:
+![Yellow text on red background](http://localhost/examples/yellow_in_red.png)
+-
+##### Yellow Bold text on red background:
+
+```
+{
+	bgColor(RED);
+	fgColor(YELLOW);
+	textBold(TRUE);
+	printf("YELLOW and BOLD text in RED Background");
+}
+```
+######Result:
+![termcolor Yellow Bold text on red background](http://localhost/examples/bold_yellow_in_red.png)
+-
+##### Yellow, Bold and underlined text on red background:
+
+```
+{
+	bgColor(RED);
+	fgColor(YELLOW);
+	textBold(TRUE);
+	textUnderline(TRUE);
+	printf("YELLOW and BOLD text in RED Background");
+}
+```
+######Result:
+![termcolor Yellow, Bold and underlined text on red background](http://localhost/examples/bold_yellow_underline.png)
+-
+
+##### High Intensity: Yellow Bold text on red background:
+
+```
+{
+	bgColor(RED);
+	fgColor(YELLOW);
+	textBold(TRUE);
+	highFgIntensity(TRUE);
+	highBgIntensity(TRUE);
+	printf("YELLOW and BOLD text in RED Background");
+}
+```
+######Result:
+![termcolor High Intensity: Yellow Bold text on red background](http://localhost/examples/high_intens.png)
+-
+
+##### Reversed colors:
+
+```
+{
+	bgColor(DEFAULT);
+	fgColor(DEFAULT);
+	textBold(TRUE);
+	textUnderline(TRUE);
+	printf("REVERSED test with default foreground and default background :)");
+}
+```
+######Result:
+![termcolor Reversed colors](http://localhost/examples/reversed_colors.png)
+-
+
+##### Main Example:
+
+```
+#include <stdio.h>
+#include "termcolor.h"
+
+int main() {
+
+	printf("Hi, Thanks for trying termcolor library :) \n" );
+	printf("Using this library allow the developer to decorate \
+the output of its console application.\n\n");
+	printf("Some Examples: \n\n");
+	
+	bgColor(RED); // Background RED and not "RED" !
+	printf("text with RED Background\n\n");
+	bgColor(DEFAULT); // Default Background Color
+	fgColor(RED); // RED Foreground color 
+	printf("text with RED Foreground\n\n");
+	bgColor(RED);
+	fgColor(YELLOW);
+	printf("YELLOW text in RED Background\n\n");
+	textBold(TRUE); // Bold Text
+	printf("YELLOW and BOLD text in RED Background\n\n");
+	textUnderline(TRUE); // Underlined text 
+	bgColor(DEFAULT);
+	printf("YELLOW and BOLD and Underlined text\n\n");
+	fgColor(DEFAULT);
+	colorReverse(TRUE);
+	printf("REVERSED test with default foreground and default background :)\n\n");
+	colorReverse(FALSE);
+	textBlink(TRUE);
+	printf("BLINKING text with default foreground and default background\n\n");
+	
+	resetStyle(); // Reset all as default 
+	printf("Now Examples using High Intensity\n\n");
+	highFgIntensity(TRUE);
+	highBgIntensity(TRUE);
+	bgColor(RED);
+	printf("text with RED Background\n\n");
+	bgColor(DEFAULT);
+	fgColor(RED);
+	printf("text with RED Foreground\n\n");
+	bgColor(RED);
+	fgColor(YELLOW);
+	printf("YELLOW text in RED Background\n\n");
+	textBold(TRUE);
+	printf("YELLOW and BOLD text in RED Background\n\n");
+	
+	resetStyle();
+	printf("Simple Text :D \n\n");
+	
+    return 0;
+}
+
+```
+######Result:
+![termcolor Reversed colors](http://localhost/examples/example_termcolor.png)
+-
+
 
 ###Who am i ?
-cyounes@twitter
+My name is [Younes Cheikh][1] C-addicted, student and desktop applications developer, follow me on [twitter][2] or visite my [personal website][3] :)
+
+
+[1]: http://github.com/cyounes/        "c.Younes on github"
+[2]: http://twitter/cyounes  "@cyounes"
+[3]: http://cyounes.com/    "personal website"
 
--- termcolor.h
+++ termcolor.h
@@ -1,19 +1,19 @@
 /***
- * Termcolor Copyright &copy; 2012  
+ * Termcolor Copyright &copy; 2012
  *
- * This program is free software: you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by the Free 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * for more informations contact at <contact@cYounes.com>
- * 
+ *
  * author: Younes Cheikh : cyounes@twitter
  * Project Page: https://github.com/cyounes/termcolor
  */
@@ -29,54 +29,95 @@
 #define _TERMCOLOR_H
 
 #include <stdio.h>
+#include <string.h>
+
+
 
 /*
- * Available colors 
+ * Available colors
  */
 typedef enum {
-  BLACK,  /*!< number 0 */
-  RED,    /*!< number 1 */
-  GREEN,  /*!< number 2 */
-  YELLOW, /*!< number 3 */
-  BLUE,   /*!< number 4 */
-  MAGENTA,/*!< number 5 */
-  CYAN,   /*!< number 6 */
-  WHITE,   /*!< number 7 */
-  DEFAULT  /*!< number 8 */ 
+    BLACK,  /*!< number 0 */
+    RED,    /*!< number 1 */
+    GREEN,  /*!< number 2 */
+    YELLOW, /*!< number 3 */
+    BLUE,   /*!< number 4 */
+    MAGENTA,/*!< number 5 */
+    CYAN,   /*!< number 6 */
+    WHITE,   /*!< number 7 */
+    DEFAULT  /*!< number 8 */
 }
 _COLOR_ ;
 
 /*
  * Type Boolean
  */
-typedef enum { 
-  FALSE, /*!< Can be the integer 0 */
-  TRUE  /*!< Can be the integer 1 */
-} 
+typedef enum {
+    FALSE, /*!< Can be the integer 0 */
+    TRUE  /*!< Can be the integer 1 */
+}
 BOOLEAN;
+
+char    * START_BOLD ="${bd}",
+* STOP_BOLD = "${/bd}",
+* START_UNDERLINE ="${ul}",
+* STOP_UNDERLINE = "${/ul}",
+* START_BLINK ="${bl}",
+* STOP_BLINK = "${/bl}",
+* START_REVERSE ="${rv}",
+* STOP_REVERSE = "${/rv}",
+* START_BG_INTENSITY ="${bi}",
+* STOP_BG_INTENSITY = "${/bi}",
+* START_FG_INTENSITY ="${fi}",
+* STOP_FG_INTENSITY = "${/fi}",
+* START_BG ="${bg}", // Start background color
+* STOP_BG = "${/bg}",  // Stop background color
+* BLACK_BACKGROUND = "${bb}",
+* RED_BACKGROUND = "${rb}",
+* GREEN_BACKGROUND = "${gb}",
+* YELLOW_BACKGROUND = "${yb}",
+* BLUE_BACKGROUND = "${ub}",
+* MAGENTA_BACKGROUND = "${mb}",
+* CYAN_BACKGROUND = "${cb}",
+* WHITE_BACKGROUND = "${wb}",
+* START_FG ="${fg}", // Start background color
+* STOP_FG = "${/fg}",  // Stop background color
+* BLACK_FOREGROUND = "${bf}",
+* RED_FOREGROUND = "${rf}",
+* GREEN_FOREGROUND = "${gf}",
+* YELLOW_FOREGROUND = "${yf}",
+* BLUE_FOREGROUND = "${uf}",
+* MAGENTA_FOREGROUND = "${mf}",
+* CYAN_FOREGROUND = "${cf}",
+* WHITE_FOREGROUND = "${wf}";
+
+//char KEYWORDS[32][];
+//= { START_BOLD , STOP_BOLD , START_UNDERLINE, STOP_UNDERLINE, START_BLINK, STOP_BLINK, START_REVERSE, STOP_REVERSE, START_BG_INTENSITY, STOP_BG_INTENSITY,START_FG_INTENSITY, STOP_FG_INTENSITY, START_BG, STOP_BG, BLACK_BACKGROUND, RED_BACKGROUND, GREEN_BACKGROUND, YELLOW_BACKGROUND, BLUE_BACKGROUND ,MAGENTA_BACKGROUND ,CYAN_BACKGROUND ,WHITE_BACKGROUND ,START_FG , STOP_FG , BLACK_FOREGROUND ,RED_FOREGROUND ,GREEN_FOREGROUND ,YELLOW_FOREGROUND ,BLUE_FOREGROUND ,MAGENTA_FOREGROUND ,CYAN_FOREGROUND ,WHITE_FOREGROUND };
+
+//char KEYWORDS[3][10] ;
 
 /*
  * Standard variables
  */
 BOOLEAN _BOLD_TEXT=FALSE,
-		_HIGH_INTENSITY_FG=FALSE,
-		_HIGH_INTENSITY_BG=FALSE,
-		_UNDERLINE_TEXT=FALSE,
-		_BLINK_TEXT=FALSE,
-		_REVERSE_COLORS=FALSE;
+_HIGH_INTENSITY_FG=FALSE,
+_HIGH_INTENSITY_BG=FALSE,
+_UNDERLINE_TEXT=FALSE,
+_BLINK_TEXT=FALSE,
+_REVERSE_COLORS=FALSE;
 
 
 int FGCOLOR, /*!< The forground chosed by the programer */
-    BGCOLOR; /*!< The background chosed by the programer */
+BGCOLOR; /*!< The background chosed by the programer */
 
-BOOLEAN _CHANGE_FGCOLOR=FALSE, /*!< this will be set TRUE if you         call the FG_COLOR_ON() function */                                          
-       _CHANGE_BGCOLOR=FALSE; /*!< this will be set TRUE if you call the           FG_COLOR_ON() function */
-    
+BOOLEAN _CHANGE_FGCOLOR=FALSE, /*!< this will be set TRUE if you         call the FG_COLOR_ON() function */
+_CHANGE_BGCOLOR=FALSE; /*!< this will be set TRUE if you call the           FG_COLOR_ON() function */
+BOOLEAN _AUTO_RESET = FALSE;
 
 /*
  * HEADERS
  */
-    
+
 void bgColor(_COLOR_ COLOR) ;
 
 void fgColor(_COLOR_ COLOR) ;
@@ -97,8 +138,13 @@ void setStyle() ;
 
 void resetStyle() ;
 
+void autoResetStyle(BOOLEAN autoReset) ;
+
+void cprint(char * string);
+
+
 /*
- * FUNCTIONS 
+ * FUNCTIONS
  */
 void bgColor(_COLOR_ COLOR) {
 	
@@ -106,7 +152,7 @@ void bgColor(_COLOR_ COLOR) {
 		BGCOLOR = (_HIGH_INTENSITY_BG==FALSE)?40:100 ;
 		BGCOLOR = BGCOLOR + COLOR;
 		_CHANGE_BGCOLOR = TRUE;
-	} 
+	}
 	else {
 		_CHANGE_BGCOLOR = FALSE;
 	}
@@ -119,7 +165,7 @@ void fgColor(_COLOR_ COLOR) {
 		FGCOLOR = (_HIGH_INTENSITY_FG==FALSE)?30:90;
 		FGCOLOR = FGCOLOR + COLOR;
 		_CHANGE_FGCOLOR = TRUE;
-	} 
+	}
 	else {
 		_CHANGE_FGCOLOR = FALSE;
 	}
@@ -127,8 +173,8 @@ void fgColor(_COLOR_ COLOR) {
 }
 
 void textBold(BOOLEAN status) {
-  _BOLD_TEXT = status ;
-  setStyle();
+    _BOLD_TEXT = status ;
+    setStyle();
 }
 
 void textBlink(BOOLEAN status) {
@@ -143,6 +189,7 @@ void colorReverse(BOOLEAN status) {
 
 void textUnderline(BOOLEAN status) {
 	_UNDERLINE_TEXT = status ;
+    setStyle();
 }
 
 void highFgIntensity(BOOLEAN status) {
@@ -164,11 +211,11 @@ void setStyle() {
 	conf[2] = (_UNDERLINE_TEXT == FALSE ) ? 0 : 4 ;
 	conf[3] = (_CHANGE_FGCOLOR == FALSE ) ? 0 : FGCOLOR ;
 	conf[4] = (_CHANGE_BGCOLOR == FALSE ) ? 0 : BGCOLOR ;
-	conf[5] = (_REVERSE_COLORS == FALSE ) ? 0 : 7 ; 
+	conf[5] = (_REVERSE_COLORS == FALSE ) ? 0 : 7 ;
 	
 	printf("\e[0m");
 	int i ;
-	for (i = 0 ; i< 6 ; i++) 
+	for (i = 0 ; i< 6 ; i++)
 	{
 		if (conf[i] !=0 )
 			printf("\e[%dm", conf[i]);
@@ -176,13 +223,92 @@ void setStyle() {
 }
 
 void resetStyle() {
-  _BLINK_TEXT = FALSE; 
-  _BOLD_TEXT = FALSE;
-  _UNDERLINE_TEXT = FALSE;
-  _REVERSE_COLORS = FALSE;
-  FGCOLOR = 0 ;
-  BGCOLOR = 0 ;
-  setStyle();
+    _BLINK_TEXT = FALSE;
+    _BOLD_TEXT = FALSE;
+    _UNDERLINE_TEXT = FALSE;
+    _REVERSE_COLORS = FALSE;
+    FGCOLOR = DEFAULT ;
+    BGCOLOR = DEFAULT ;
+    _HIGH_INTENSITY_BG = FALSE;
+    _HIGH_INTENSITY_FG = FALSE;
+    setStyle();
 }
+
+void autoResetStyle(BOOLEAN autoReset) {
+    _AUTO_RESET = autoReset;
+}
+
+
+void cprint(char * string ) {
+    int i = 0 ,j;
+    char tmpString[7];
+    while (i < strlen(string)) {
+        if (string[i]=='$') {
+            for (j=0; j<6;j++) {
+                tmpString[j] = string[i+j];
+            }
+            if (tmpString[2] != '/') {
+                tmpString[5] = '\0';
+                
+                // DECORATION
+                if (strcmp(tmpString, START_BOLD) == 0 ) textBold(TRUE);
+                else if (strcmp(tmpString, START_BLINK) == 0 ) textBlink(TRUE);
+                else if (strcmp(tmpString, START_UNDERLINE) == 0 ) textUnderline(TRUE);
+                else if (strcmp(tmpString, START_BG_INTENSITY) == 0 ) highBgIntensity(TRUE);
+                else if (strcmp(tmpString, START_FG_INTENSITY) == 0  ) highFgIntensity(TRUE);
+                else if (strcmp(tmpString, START_REVERSE) == 0 ) colorReverse(TRUE);
+                else {
+                    
+                    // BACKGROUND COLOR
+                    BGCOLOR = (strcmp(tmpString, START_BG) == 0 ) ? DEFAULT : BGCOLOR ;
+                    BGCOLOR = (strcmp(tmpString, BLACK_BACKGROUND) == 0 ) ? BLACK : BGCOLOR ;
+                    BGCOLOR = (strcmp(tmpString, RED_BACKGROUND) == 0 ) ? RED : BGCOLOR ;
+                    BGCOLOR = (strcmp(tmpString, GREEN_BACKGROUND) == 0 ) ? GREEN : BGCOLOR ;
+                    BGCOLOR = (strcmp(tmpString, YELLOW_BACKGROUND) == 0 ) ? YELLOW : BGCOLOR ;
+                    BGCOLOR = (strcmp(tmpString, BLUE_BACKGROUND) == 0 ) ? BLUE : BGCOLOR ;
+                    BGCOLOR = (strcmp(tmpString, MAGENTA_BACKGROUND) == 0 ) ? MAGENTA : BGCOLOR ;
+                    BGCOLOR = (strcmp(tmpString, CYAN_BACKGROUND) == 0 ) ? CYAN : BGCOLOR ;
+                    BGCOLOR = (strcmp(tmpString, WHITE_BACKGROUND) == 0 ) ? WHITE : BGCOLOR ;
+                    
+                    //FOREGROUND COLOR
+                    FGCOLOR = (strcmp(tmpString, START_FG) == 0 ) ? DEFAULT : FGCOLOR ;
+                    FGCOLOR = (strcmp(tmpString, BLACK_FOREGROUND) == 0 ) ? BLACK : FGCOLOR ;
+                    FGCOLOR = (strcmp(tmpString, RED_FOREGROUND) == 0 ) ? RED : FGCOLOR ;
+                    FGCOLOR = (strcmp(tmpString, GREEN_FOREGROUND) == 0 ) ? GREEN : FGCOLOR ;
+                    FGCOLOR = (strcmp(tmpString, YELLOW_FOREGROUND) == 0 ) ? YELLOW : FGCOLOR ;
+                    FGCOLOR = (strcmp(tmpString, BLUE_FOREGROUND) == 0 ) ? BLUE : FGCOLOR ;
+                    FGCOLOR = (strcmp(tmpString, MAGENTA_FOREGROUND) == 0 ) ? MAGENTA : FGCOLOR ;
+                    FGCOLOR = (strcmp(tmpString, CYAN_FOREGROUND) == 0 ) ? CYAN : FGCOLOR ;
+                    FGCOLOR = (strcmp(tmpString, WHITE_FOREGROUND) == 0 ) ? WHITE : FGCOLOR ;
+                    
+                    //APPLY NEW DECORE
+                    bgColor(BGCOLOR);
+                    fgColor(FGCOLOR);
+                }
+                i= i+5 ;
+            }
+            else {
+                if (strcmp(tmpString, STOP_BOLD) == 0 ) textBold(FALSE);
+                else if (strcmp(tmpString, STOP_UNDERLINE) == 0 ) textUnderline(FALSE);
+                else if (strcmp(tmpString, STOP_BLINK) == 0 ) textBlink(FALSE);
+                else if (strcmp(tmpString, STOP_REVERSE) == 0 ) colorReverse(FALSE);
+                else if (strcmp(tmpString, STOP_BG_INTENSITY) == 0 ) highBgIntensity(FALSE);
+                else if (strcmp(tmpString, STOP_FG_INTENSITY) == 0 ) highFgIntensity(FALSE);
+                else if (strcmp(tmpString, STOP_BG) == 0 ) bgColor(DEFAULT);
+                else if (strcmp(tmpString, STOP_FG) == 0 ) fgColor(DEFAULT);
+                
+                i= i+6 ;
+            }
+        }
+        else {
+            printf("%c",string[i]);
+            i++;
+        }
+    }
+    if (_AUTO_RESET)
+        resetStyle();
+}
+
+
 
 #endif
